7a782d71458c193c183296aa570957bbf98dd67a
Michal Škoula <michal@skoula.com>
2025-06-09 16:01:15 +0200

    Create vite project with tailwind

---
+++ src/main.js
@@ -0,0 +1,47 @@
+import "./style.css";
+
+// Elements setup
+
+/**
+ * Top bar with the logo, links, call to action etc.
+ */
+const navbar = document.getElementById("navbar") ?? null;
+
+/**
+ * Sidebar that opens when you click the hamburger menu
+ */
+const slideover = document.getElementById("slideover") ?? null;
+
+/**
+ * Button that opens the slideover; inside of the navbar
+ */
+const openBtn = document.getElementById("slideoverOpenBtn") ?? null;
+
+/**
+ * Button that closes the slideover; inside of the slideover itself
+ */
+const closeBtn = document.getElementById("slideoverCloseBtn") ?? null;
+
+// Hide navbar on scroll
+let lastScrollPos = 0;
+window.addEventListener("scroll", () => {
+  let currScrollPos = window.scrollY;
+
+  lastScrollPos > currScrollPos
+    ? navbar?.classList.add("collapsed")
+    : navbar?.classList.remove("collapsed");
+
+  lastScrollPos = currScrollPos;
+});
+
+// Open and close the slideover
+
+openBtn?.addEventListener("click", () => {});
+closeBtn?.addEventListener("click", () => {});
+
+function openSlideover() {
+  slideover.classList.add("open");
+}
+function closeSlideover() {
+  slideover.classList.remove("open");
+}
